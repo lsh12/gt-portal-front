59e89cfc1488f58d6f500ecb3cdde2dc96967184
becky <34289742+beckyjang@users.noreply.github.com>
2018-11-08 17:02:33 +0900

    add translate module

--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -1,35 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
 import { MockService } from 'src/app/services/mock.service';
+import { TranslateService } from '@ngstack/translate';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-  
-  current_user:any={};
-  current_user_role:any={};
 
-  constructor(private userService:UserService,
-              private mockService:MockService
-    ) { }
+export class HeaderComponent implements OnInit {
+  languages = [
+    { code: 'en', label: 'English' },
+    { code: 'ko', label: '한국어' },
+  ];
+  current_lang: string;
+
+  current_user: any = {};
+  current_user_role: any = {};
+
+  constructor(private userService: UserService,
+              private mockService: MockService,
+              private translate: TranslateService
+  ) { }
 
   ngOnInit() {
-    this.userService.getUserSession().subscribe(data =>{
-      this.current_user=data['user'];
-      this.current_user_role=data['role'];
+    this.userService.getUserSession().subscribe(data => {
+      this.current_user = data['user'];
+      this.current_user_role = data['role'];
     }
-    ,
-    (err)=>{
-        console.log(err,err.message);
+      ,
+      (err) => {
+        console.log(err, err.message);
       }
     );
-    
+    this.current_lang = this.translate.activeLang;
+    console.log("translate.activeLang:" + this.translate.activeLang);
   }
 
-
+  changeLang(lang: string) {
+    this.translate.activeLang = lang;
+    this.current_lang = lang;
+  }
 
 
 
